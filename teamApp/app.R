b58e6f3711b4022eb93214422aac5f4ccb340476
onpyphpt <ophetchpitak@ucdavis.edu>
2026-04-23 15:44:33 -0700

    just a bit minor

--- teamApp/app.R
+++ teamApp/app.R
@@ -5,7 +5,6 @@ library(tidyverse)
 # data loading and one-time processing here
 data.pheno <- read_csv("teamApp/RiceDiversity.44K.MSU6.Phenotypes.csv")
 
-
 # Define UI for application 
 ui <- fluidPage( #create the overall page
     #UI code here
@@ -15,6 +14,20 @@ ui <- fluidPage( #create the overall page
 # Define server logic 
 server <- function(input, output) {
   # server code here
+  
+  # pivoting FT data for histogram
+  data.pheno.FT <- data.pheno %>% 
+    select('Region', 
+           'Flowering time at Arkansas',
+           'Flowering time at Faridpur',
+           'Flowering time at Aberdeen',
+           'Seed length') %>% 
+    pivot_longer(cols = c(-'Region',-'Seed length'),
+                 names_to = 'location',
+                 names_prefix = 'Flowering time at ',
+                 values_to = 'FT')
+  
+  
 }
 
 # Run the application 
