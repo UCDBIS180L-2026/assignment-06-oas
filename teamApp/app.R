a4f8773d5cbe2596294e10124b274e6820f561ec
Shefali Baweja <shbaweja@ucdavis.edu>
2026-04-23 16:04:16 -0700

    change readcsv filepath

--- teamApp/app.R
+++ teamApp/app.R
@@ -3,7 +3,7 @@ library(tidyverse)
 # other libraries here
 
 # data loading and one-time processing here
-data.pheno <- read_csv("teamApp/RiceDiversity.44K.MSU6.Phenotypes.csv")
+data.pheno <- read_csv("RiceDiversity.44K.MSU6.Phenotypes.csv")
 
 # Define UI for application 
 ui <- fluidPage( #create the overall page
